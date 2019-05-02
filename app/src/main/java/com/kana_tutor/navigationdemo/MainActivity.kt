eae2828e83cc609c9fd68976623f9342fab3305f
sjs <sjs@sonic.net>
2019-05-01 21:06:56 -0700

    Step01-min-gradle-changes-for-databinding

--- app/src/main/java/com/kana_tutor/navigationdemo/MainActivity.kt
+++ app/src/main/java/com/kana_tutor/navigationdemo/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.kana_tutor.navigationdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
